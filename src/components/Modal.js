4806f84c4643d36d06e442c1e6cffb9732c454d5
“vukas86” <vukasbit@gmail.com>
2023-11-15 23:46:15 +0100

    work on modal, add button and images in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,66 @@
 import styles from "./Modal.module.css";
 
+import rocket1 from "../assets/bg/rocket1.jpg";
+import rocket2 from "../assets/bg/rocket2.jpg";
+import { useState } from "react";
+
 function Modal({ closeModal }) {
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  const handleCloseModal = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains(styles.modalBackground)) {
+      closeModal(false);
+    }
+  };
+
+  const handleImageClick = (index) => {
+    setSelectedImage(index);
+  };
+
+  const images = [rocket1, rocket2];
+
   return (
-    <div className={styles.modalBackground}>
+    <div className={styles.modalBackground} onClick={handleCloseModal}>
       <div className={styles.modalContainer}>
-        <h1>
-          Name: <span>Falcon</span>
-        </h1>
-        <h3>
-          Type: <span>Rocket</span>
-        </h3>
-        <h3>
-          Country: <span>USA</span>
-        </h3>
-        <h3>
-          First Flight: <span>05/12/2021</span>
-        </h3>
-        <button onClick={() => closeModal(false)}> Close </button>
+        <section className={styles.infoPart}>
+          <h1>
+            Name: <span>Falcon</span>
+          </h1>
+          <p>
+            Type: <span>Rocket</span>
+          </p>
+          <p>
+            Country: <span>USA</span>
+          </p>
+          <p>
+            First Flight: <span>05/12/2021</span>
+          </p>
+          <button onClick={() => closeModal(false)}> Close </button>
+        </section>
+        <div className={styles.imagesSection}>
+          {selectedImage !== null && (
+            <section className={styles.selectedImageSection}>
+              <img
+                src={images[selectedImage]}
+                alt={`Rocket ${selectedImage + 1}`}
+                className={styles.selectedImage}
+              />
+            </section>
+          )}
+          <section className={styles.imagesPart}>
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Rocket ${index + 1}`}
+                // className={selectedImage === index ? styles.selectedImage : ""}
+                className={styles.tmbImg}
+                onClick={() => handleImageClick(index)}
+              />
+            ))}
+          </section>
+        </div>
       </div>
     </div>
   );
