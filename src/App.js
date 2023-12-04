6149ce75d7206631bd2ceb321060b634a5782de9
“vukas86” <vukasbit@gmail.com>
2023-12-04 19:25:08 +0100

    adding loader

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { Suspense } from "react";
+
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner/Banner";
 import Search from "./components/Search";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 import ErrorBoundary from "./utils/ErrorBoundary";
+import Loader from "./components/Loader";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <ErrorBoundary>
-        <Banner />
-      </ErrorBoundary>
-      <Search />
-      <Main />
-      <Footer />
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className="App">
+        <Navbar />
+        <ErrorBoundary>
+          <Banner />
+        </ErrorBoundary>
+        <Search />
+        <Main />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
 
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div>
+      <span class="loader"></span>
+    </div>
+  );
+}
+
+export default Loader;
